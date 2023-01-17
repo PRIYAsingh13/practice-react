ff49549dac3d9c45fd292191659dea0f1497f3e6
PRIYAsingh13 <105417945+PRIYAsingh13@users.noreply.github.com>
2023-01-17 13:01:55 +0800

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Home extends React.Component{
+    render()
+    {
+        return(
+            <div>
+                <h1>priya</h1>
+                <h2>priya2</h2>
+            </div>
+        )
+    }
+}
